6bfb277b87b3f2c363cfe1803e913ce26d1519a3
Warner Lin <WarnerWeiLin@gmail.com>
2020-09-15 12:36:03 -0500

    Added Reducer Functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'
+import reducers from './reducers';
 import logo from './logo.svg';
 import './App.css';
 
+const store = createStore(reducers);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
